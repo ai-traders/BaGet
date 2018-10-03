ee8f631e5de40fb9f48d98f974b64f045e910858
Tomasz Setkowski <tom@ai-traders.com>
2018-10-03 18:33:53 +0000

    move models into separate namespace to reuse with cache controllers

--- src/BaGet.Web/Controllers/Registration/RegistrationLeafController.cs
+++ src/BaGet.Web/Controllers/Registration/RegistrationLeafController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using BaGet.Core.Mirror;
 using BaGet.Core.Services;
 using BaGet.Web.Extensions;
+using BaGet.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using NuGet.Versioning;
@@ -53,39 +54,6 @@ namespace BaGet.Controllers.Web.Registration
                 registrationIndexUri: Url.PackageRegistration(id));
 
             return Json(result);
-        }
-
-        public class RegistrationLeaf
-        {
-            public RegistrationLeaf(
-                string registrationUri,
-                bool listed,
-                long downloads,
-                string packageContentUri,
-                DateTimeOffset published,
-                string registrationIndexUri)
-            {
-                RegistrationUri = registrationUri ?? throw new ArgumentNullException(nameof(registrationIndexUri));
-                Listed = listed;
-                Published = published;
-                Downloads = downloads;
-                PackageContent = packageContentUri ?? throw new ArgumentNullException(nameof(packageContentUri));
-                RegistrationIndexUri = registrationIndexUri ?? throw new ArgumentNullException(nameof(registrationIndexUri));
-            }
-
-            [JsonProperty(PropertyName = "@id")]
-            public string RegistrationUri { get; }
-
-            public bool Listed { get; }
-
-            public long Downloads { get; }
-
-            public string PackageContent { get; }
-
-            public DateTimeOffset Published { get; }
-
-            [JsonProperty(PropertyName = "registration")]
-            public string RegistrationIndexUri { get; }
         }
     }
 }
--- src/BaGet.Web/Controllers/SearchController.cs
+++ src/BaGet.Web/Controllers/SearchController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using BaGet.Core.Services;
 using BaGet.Web.Extensions;
+using BaGet.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -40,61 +41,6 @@ namespace BaGet.Web.Controllers
                 TotalHits = results.Count,
                 Data = results,
             });
-        }
-
-        private class SearchResultModel
-        {
-            private readonly SearchResult _result;
-            private readonly IUrlHelper _url;
-
-            public SearchResultModel(SearchResult result, IUrlHelper url)
-            {
-                _result = result ?? throw new ArgumentNullException(nameof(result));
-                _url = url ?? throw new ArgumentNullException(nameof(url));
-
-                var versions = result.Versions.Select(
-                    v => new SearchResultVersionModel(
-                        url.PackageRegistration(result.Id, v.Version),
-                        v.Version.ToNormalizedString(),
-                        v.Downloads));
-
-                Versions = versions.ToList().AsReadOnly();
-            }
-
-            public string Id => _result.Id;
-            public string Version => _result.Version.ToNormalizedString();
-            public string Description => _result.Description;
-            public string Authors => _result.Authors;
-            public string IconUrl => _result.IconUrl;
-            public string LicenseUrl => _result.LicenseUrl;
-            public string ProjectUrl => _result.ProjectUrl;
-            public string Registration => _url.PackageRegistration(_result.Id);
-            public string Summary => _result.Summary;
-            public string[] Tags => _result.Tags;
-            public string Title => _result.Title;
-            public long TotalDownloads => _result.TotalDownloads;
-
-            public IReadOnlyList<SearchResultVersionModel> Versions { get; }
-        }
-
-        private class SearchResultVersionModel
-        {
-            public SearchResultVersionModel(string registrationUrl, string version, long downloads)
-            {
-                if (string.IsNullOrEmpty(registrationUrl)) throw new ArgumentNullException(nameof(registrationUrl));
-                if (string.IsNullOrEmpty(version)) throw new ArgumentNullException(nameof(version));
-
-                RegistrationUrl = registrationUrl;
-                Version = version;
-                Downloads = downloads;
-            }
-
-            [JsonProperty(PropertyName = "id")]
-            public string RegistrationUrl { get; }
-
-            public string Version { get; }
-
-            public long Downloads { get; }
         }
     }
 }
